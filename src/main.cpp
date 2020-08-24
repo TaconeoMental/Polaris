9f53c10ca48d1bcdacd0c25d59f93b6405b1e4b3
Mateo Contenla <mcontenlaf@gmail.com>
2020-08-24 11:05:33 -0400

    Cambio de color secundario cercano

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 #define DELAYVAL 500
 
 Color color_cerca_principal(150, 0, 0);
-Color color_cerca_secundario(0, 250, 0);
+Color color_cerca_secundario(250, 150, 0);
 Color color_lejos_principal(150, 0, 0);
 Color color_lejos_secundario(150, 0, 0);
 
@@ -23,6 +23,7 @@ void setup() {
     #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
         clock_prescale_set(clock_div_1);
     #endif
+    brujula_led.actualizarGrado(0, 100);
 }
 
 void loop() {
